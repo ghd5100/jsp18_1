5981b24222f3753cfac74d7ab36b8e7d92109a53
ghd5100 <ghd5100@gmail.com>
2022-01-06 16:19:13 +0900

    main step5. 노티스 컨트롤러. 메인 리스트까지만 뿌릴수있게 작업

--- src/customer/controller/notice/NoticeController.java
+++ src/customer/controller/notice/NoticeController.java
@@ -1,9 +1,13 @@
 package customer.controller.notice;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import customer.controller.Controller;
+import customer.dao.NoticeDao;
+import customer.vo.Notice;
 
 public class NoticeController implements Controller {
 	@Override
@@ -11,6 +15,22 @@ public class NoticeController implements Controller {
 			throws Exception {
 		System.out.println("NoticeController~");
 
+		
+		NoticeDao dao=new NoticeDao();
+		 List<Notice> list=dao.noticeSelAll();
+		
+	
+		/*List<Notice>를 request에 저장해보쟈.***/
+		//목적은: 검색하고나면 검색기록이 안사라지도록 하기위함이다.
+		request.setAttribute("list", list);
+
+		
+
+		
+		/*포워딩하면 자료를 살려서 보내줄수있다. 샌드는 자료가 없어져서 안돼고*/
+		request.getRequestDispatcher("notice.jsp").forward(request, response);
+		/*포워드 해주는 것들에는 list .query 가있따. 담아준만큼 다 보내준다*/
+		
 	}
 }
 
--- src/customer/dao/NoticeDao.java
+++ src/customer/dao/NoticeDao.java
@@ -1,16 +1,48 @@
 package customer.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
+import customer.db.DBCon;
 import customer.vo.Notice;
 
 public class NoticeDao {
 	
 	public List<Notice> noticeSelAll() throws Exception {
-		return null;
+		//보따리 속에 또 보따리르 쌓아야함... 이 매소드는. 리스트에다가 클래스를 담아쥼요!
+		
+		Connection con=DBCon.getConnection();
+		String sql="select * from notices order by to_number(seq) desc";
 		
 		
+		//실행
+		PreparedStatement pstmt=con.prepareStatement(sql);
 
+		/*쿼리를 넣는데 대신 %자바%니까 ..%포함되게 */
+		
+		//결과
+		ResultSet rs=pstmt.executeQuery();
+		/*pstmt는 익스큐트쿼리괄호에 sql안들어감*/
+		
+		
+		//리스트에 Notice 내용을 담기 *** 잘 이해해보기
+		List<Notice> list=new ArrayList<Notice>();
+		
+		while (rs.next()) { //다음있을떄 까지 계속 돈다. if 절을 안에 만들필요는 없어... while문 자체가 참일떄 계속 도니까.
+			Notice n=new Notice();
+			n.setSeq(rs.getString("seq"));
+			n.setContent(rs.getString("content"));
+			n.setHit(rs.getInt("hit"));
+			n.setWriter(rs.getString("writer"));
+			n.setRegdate(rs.getDate("regdate"));
+			n.setTitle(rs.getString("title"));
+			list.add(n);
+		}
+		
+		return list;
 	}
 	
 
